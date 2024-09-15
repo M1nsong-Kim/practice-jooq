6c29e3a9b80315e0504dbc30d8dda8a878525d47
M1nsong-Kim <lgms.3222@gmail.com>
2024-09-15 19:12:24 +0900

    practice-jooq project

---
+++ src/test/java/com/practice/practice_jooq/PracticeJooqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.practice_jooq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeJooqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
