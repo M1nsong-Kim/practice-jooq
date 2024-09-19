16cd6b6725e45448b2ddc893e61ebe55311a97ae
M1nsong-Kim <lgms.3222@gmail.com>
2024-09-19 20:51:29 +0900

    gradle: jooq 설정 수정 및 build 관련 오류 잡기

--- src/main/java/com/practice/practice_jooq/PracticeJooqApplication.java
+++ src/main/java/com/practice/practice_jooq/PracticeJooqApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// @EntityScan(basePackages = "com.practice.practice_jooq.member")	// PracticeJooqApplicationTests > contextLoads() FAILED  조치였지만 해결X
 public class PracticeJooqApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/practice/practice_jooq/PracticeJooqApplicationTests.java
+++ src/test/java/com/practice/practice_jooq/PracticeJooqApplicationTests.java
@@ -1,13 +1,13 @@
 package com.practice.practice_jooq;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class PracticeJooqApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+	// gradle build 시 났던 오류
+	/*	1. PracticeJooqApplicationTests > initializationError FAILED
+	    	java.lang.IllegalStateException at Assert.java:76
+	 * 	2. PracticeJooqApplicationTests > contextLoads() FAILED
+		    java.lang.IllegalStateException at DefaultCacheAwareContextLoaderDelegate.java:180
+		        Caused by: org.springframework.beans.factory.BeanCreationException at AbstractAutowireCapableBeanFactory.java:1806
+		            Caused by: org.hibernate.AnnotationException at InheritanceState.java:279
+	 */
+	// 테스트 코드에 아무것도 없을 때 @SpringBootTest 어노테이션을 없애면 된다
 }
